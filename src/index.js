0f34acf9656a8dc40fec1b4b71c97cbc67eaaf01
Ben <benh@veriti.ai>
2024-11-27 16:21:47 +0200

    small changes for testing

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const cognitoAuthConfig = {
   authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_xNFuVanTz",
   client_id: "67ofutr22ug4bbklq6dd00elh9",
   // redirect_uri: "http://localhost:3000",
-  redirect_uri: "https://main.d2nt8ipaswef7v.amplifyapp.com/",
+  // redirect_uri: "https://main.d2nt8ipaswef7v.amplifyapp.com/",
+  redirect_uri: process.env.REACT_APP_REDIRECT_URL,
   response_type: "code",
   scope: "email openid phone",
 };
